a548ca1981840ed9cd762d35bd9dda9a47a50e7e
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2024-04-05 10:27:44 +0530

    Add files via upload

---
+++ Solutions/0268.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        return int(len(nums)*(len(nums)+1)/2 - sum(nums))
+'''class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(0, len(nums)+1):
+            if i not in nums:
+                return i'''
