259aba929ae345ba7c632899514ff7aa7dd625d5
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2024-04-05 10:18:47 +0530

    Update 0268.py

--- 0268.py
+++ 0268.py
@@ -1,6 +1,9 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        #sum of all numbers upto len(nums) - sum of numbers in the array nums will give you the required answer
         return int(len(nums)*(len(nums)+1)/2 - sum(nums))
+
+#my first solution which is inefficient
 '''class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         nums.sort()
