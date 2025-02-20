6b386a8562ff2e25c11c56e63781903f2c116aec
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2025-02-20 09:20:11 +0530

    28

--- Solutions/0028.py
+++ Solutions/0028.py
@@ -1,7 +1,14 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        try:
-            ans = haystack.index(needle)
-        except:
-            ans = -1
-        return ans
+
+    # this is cheating
+    # def strStr(self, haystack: str, needle: str) -> int:
+    #     try:
+    #         ans = haystack.index(needle)
+    #     except:
+    #         ans = -1
+    #     return ans
+    def strStr(self, haystack, needle):
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
