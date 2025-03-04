2d4765e14d88ed96b6086b045921c26e4f8c8e0d
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2025-03-04 07:50:10 +0530

    new changes

--- Solutions/1780.py
+++ Solutions/1780.py
@@ -1,9 +1,23 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
-
-        while n:
-            if n % 3 == 2:
-                return False
-            n = n // 3
+    
+    # - math approach (most optimal)
+        # while n:
+        #     if n % 3 == 2:
+        #         return False
+        #     n = n // 3
         
-        return True
+        # return True
+
+    # - recursive approach
+
+        def backtrack(i, sum):
+            if sum == n:
+                return True
+            if sum > n or 3 ** i > n:
+                return False
+            if backtrack(i + 1, sum + 3 ** i): return True
+            return backtrack(i + 1, sum)
+
+        return backtrack(0, 0)
+
