32e5f1993b99ed0ab4d7708abbcbe169afdddedc
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2025-03-06 08:09:26 +0530

    50

--- Solutions/0050.py
+++ Solutions/0050.py
@@ -6,8 +6,20 @@ class Solution:
             if not n:
                 return 1
             res = helper(x, n // 2)
+            print(res)
             res = res * res
             return x * res if n % 2 else res
         if n < 0:
             return 1 / helper(x, abs(n))
         return helper(x, n)
+
+        # res = 1
+        # p = abs(n)
+        # while p > 0:
+        #     if p & 1:
+        #         res = res * x
+        #     x *= x
+        #     p >>= 1
+        # if n < 0:
+        #     return 1 / res
+        # return res
