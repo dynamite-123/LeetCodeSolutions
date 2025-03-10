99e665a738616e529393072e7797c40c08136de3
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2025-03-10 16:15:10 +0530

    changes

--- Solutions/0338.py
+++ Solutions/0338.py
@@ -1,24 +1,20 @@
 class Solution:
     def countBits(self, n: int) -> List[int]: # type: ignore
         # my approach
-        # cache = {}
-        # res = [0] * (n + 1)
-        # for i in range(1, n + 1):
-        #     ones = 0
-        #     t = i
-        #     while t:
-        #         if t in cache:
-        #             ones += cache[t]
-        #             break
-        #         if t % 2:
-        #             ones += 1
-        #         t = t // 2
-        #     res[i] = ones
-        # return res
         res = [0] * (n + 1)
-        sub = 1
         for i in range(1, n + 1):
-            if sub * 2 == i:
-                sub = i
-            res[i] = res[i - sub] + 1
+            ones = 0
+            if i % 2:
+                ones += 1
+            ones += res[i // 2]
+            res[i] = ones
         return res
+
+        ##another way
+        # res = [0] * (n + 1)
+        # sub = 1
+        # for i in range(1, n + 1):
+        #     if sub * 2 == i:
+        #         sub = i
+        #     res[i] = res[i - sub] + 1
+        # return res
