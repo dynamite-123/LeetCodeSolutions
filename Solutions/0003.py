e83701b4f92664f75f4e48fa6a47da77d916bdd5
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2025-03-19 07:04:10 +0530

    0003

---
+++ Solutions/0003.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        sc = set()
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            if s[r] in sc:
+                while s[l] != s[r]: 
+                    sc.remove(s[l])
+                    l += 1
+                sc.remove(s[l])
+                l += 1
+            sc.add(s[r])
+            res = max(res, r - l + 1)
+        return res
