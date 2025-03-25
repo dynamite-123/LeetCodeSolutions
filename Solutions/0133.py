c089728bf49655215ddc23af1069fa17d8d20a17
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2025-03-25 12:03:34 +0530

    graph bfs

--- Solutions/0133.py
+++ Solutions/0133.py
@@ -8,7 +8,8 @@ class Node:
 
 from typing import Optional
 class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']: #type: ignore
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']: # type: ignore
+        # - DFS
         if not node: return None
         hm = {}
 
@@ -16,10 +17,26 @@ class Solution:
             if node in hm:
                 return hm[node]
         
-            new_node = Node(node.val)
+            new_node = Node(node.val) # type: ignore
             hm[node] = new_node
             for nei in node.neighbors:
                 new_node.neighbors.append(dfs(nei))
 
             return hm[node]
         return dfs(node)
+
+        # - BFS
+        # if not node: return None
+        # hm = {}
+        # hm[node] = Node(node.val) # type: ignore
+        # q = deque([node]) # type: ignore
+
+        # while q:
+        #     n = q.popleft()
+        #     for nei in n.neighbors:
+        #         if nei not in hm:
+        #             hm[nei] = Node(nei.val) # type: ignore
+        #             q.append(nei)
+        #         hm[n].neighbors.append(hm[nei])
+
+        # return hm[node]
