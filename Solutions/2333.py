d4ae787f69817ff35208fe855db7020d2c923807
dynamite-123 <108745890+dynamite-123@users.noreply.github.com>
2025-06-29 09:02:24 +0530

    renewed

--- Solutions/2333.py
+++ Solutions/2333.py
@@ -1,42 +1,60 @@
 from typing import List
+from itertools import accumulate
+import bisect
+
 class Solution:
     def minSumSquareDiff(self, nums1: List[int], nums2: List[int], k1: int, k2: int) -> int:
-        # binary search solution 
-        # time complexity: O(N * logT) (T = range of delta)
-        # space complexity: O(N)
-        N, k = len(nums1), k1 + k2
+        k = k1 + k2
+        N = len(nums1)
 
-        delta = [abs(i-j) for i, j in zip(nums1, nums2)]
-        delta.sort()
+        diff = sorted([abs(i - j) for i, j in zip(nums1, nums2)])
 
-        def calc(target):
-            ops = 0
-            for n in delta:
-                if n > target:
-                    ops += n - target
-            return ops
-        def good(target):
-            return calc(target) <= k
+        # pref = [0] * (N + 1)
+        # s = 0
+        # for i, n in enumerate(diff):
+        #     s += n
+        #     pref[i + 1] = s
 
-        l, r = 0, max(delta)
+        # PREFIX SUM IN SINGLE LINE
+        pref = [0] + list(accumulate(diff))
+        
+        sm = sum(diff)
+        def ops(x):
+            # MANUAL BINARY SEARCH OVER DIFF
+            # l, r = 0, N
+            # while l < r:
+            #     mid = (l + r) // 2
+            #     if diff[mid] > x:
+            #         r = mid
+            #     elif diff[mid] <= x:
+            #         l = mid + 1
+
+            # BINARY SEARCH USING INBUILT FUNCTION
+            l = bisect.bisect_right(diff, x, lo=0, hi=N)
+            
+            sub = (N - l) * x
+            return k - (sm - pref[l] - sub)
+
+        l, r = 0, max(diff)
 
         while l < r:
             mid = (l + r) // 2
 
-            if good(mid):
+            if ops(mid) >= 0:
                 r = mid
-
             else:
                 l = mid + 1
-        
-        krem = k - calc(r)
 
         res = 0
-        for x in delta:
-            if x > r:
-                x = r
-            if x == r and krem > 0 and x > 0:
-                x -= 1
-                krem -= 1
+        rem = ops(l)
+
+        for x in diff:
+            if x > l:
+                x = l
+            if x == l and rem > 0 and x > 0:
+                rem -= 1
+                x = x - 1
             res += x**2
+
         return res
+            
